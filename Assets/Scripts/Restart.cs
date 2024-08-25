1ecf1b67d246b162842730c693aa0b3537152ac3
GraphicEdit <contact@graphicedit.com>
2024-08-25 23:18:59 +0100

    gameover and youwin screens

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public void RestartGame()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+}
