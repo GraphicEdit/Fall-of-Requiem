09b557df16392d4f20e134ccca4c9a69d1915bc7
GraphicEdit <contact@graphicedit.com>
2024-08-28 20:30:02 +0100

    mx_theend and sfx_ui_gameover

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class Restart : MonoBehaviour
 {
+
+
+
     public void RestartGame()
     {
+
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 
